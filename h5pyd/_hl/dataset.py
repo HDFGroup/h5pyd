b384946fd2c0b7fccbfae2fe096e127fc70b9d74
jreadey <jreadey@hdfgroup.org>
2018-02-18 20:19:39 -0800

    remove debug line

--- h5pyd/_hl/dataset.py
+++ h5pyd/_hl/dataset.py
@@ -1170,7 +1170,6 @@ class Dataset(HLObject):
             val2 = numpy.empty(selection.mshape, dtype=val.dtype)
             val2[...] = val
             val = val2
-            self.log.debug("updated val: {}".format(val))
             mshape = val.shape
 
         # Perform the write, with broadcasting
