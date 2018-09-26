108e0af99d373b858df82b446a53572e62abd748
John Readey <jreadey@hdfgroup.org>
2018-09-25 18:37:11 -0700

    revised writedatacube example

--- examples/writedatacube.py
+++ examples/writedatacube.py
@@ -10,43 +10,59 @@
 # request a copy from help@hdfgroup.org.                                     #
 ##############################################################################
 import sys
+import os
 import h5pyd
 import numpy as np
 
-cube_side = 256
-USER_NAME = "test_user1"
-USER_PASSWD = "test"
-ENDPOINT = "http://192.168.99.100:5101"
 
-if len(sys.argv) > 1:
-    cube_side = int(sys.argv[1])
+dims = [16, 16, 16]
 
-filename = "cube_" + str(cube_side) + "_" + str(cube_side) + "_" + str(cube_side)
-filename += ".h5pyd_test.hdfgroup.org"
-#filename += ".h5"
+if len(sys.argv) < 2 or sys.argv[1] in ("-h", "--help"):
+    print("usage: python writedatacube.py <hspath> [h] [w] [d]")
+    print("example: python writedatacube.py /home/joebob 16")
+    sys.exit(1)
+
+folder = sys.argv[1]
+
+if len(sys.argv) > 2:
+    dims[0] = int(sys.argv[2])
+    dims[1] = dims[2] = dims[0]
+
+if len(sys.argv) > 3:
+    dims[1] = int(sys.argv[3])
+    dims[2] = dims[0]
+
+if len(sys.argv) > 4:
+    dims[2] = int(sys.argv[4])
+
+filename = os.path.join(folder, "cube")
+for s in dims:
+    filename += '_' + str(s)
+filename += ".h5"
 print("filename:", filename)
 
-f = h5pyd.File(filename, "w", username=USER_NAME, password=USER_PASSWD, endpoint=ENDPOINT)
-#f = h5pyd.File(filename, "w")
 
-print("filename,", f.filename)
+f = h5pyd.File(filename, "w")   
 
 print("create dataset")
 
-dset = f.create_dataset('dset', (cube_side, cube_side, cube_side),  dtype='int8')
+dset = f.create_dataset('dset', dims,  dtype='int8')
 
 print("name:", dset.name)
 print("shape:", dset.shape)
+print("chunks:", dset.chunks)
 print("dset.type:", dset.dtype)
 print("dset.maxshape:", dset.maxshape)
+print("bytes per slice:", dims[0]*dims[1])
 
 print("writing data...")
 
-for i in range(cube_side):
-    arr = np.zeros((cube_side, cube_side), dtype=dset.dtype)
-    mid = cube_side // 2
-    arr[mid,:] = 22
-    arr[:,mid] = 44
+for i in range(dims[2]):
+    arr = np.zeros((dims[0], dims[1]), dtype=dset.dtype)
+    mid_x= dims[0] // 2
+    mid_y = dims[1] // 2
+    arr[mid_x,:] = 22
+    arr[:,mid_y] = 44
     dset[i,:,:] = arr
 print("done!")
 
