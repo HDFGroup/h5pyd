d3faaaf3e42c848db09df342aeb4414a8a98ff40
John Readey <jreadey@hdfgroup.org>
2019-01-30 10:44:06 -0800

    added Table class

---
+++ h5pyd/_hl/table.py
@@ -0,0 +1,293 @@
+##############################################################################
+# Copyright by The HDF Group.                                                #
+# All rights reserved.                                                       #
+#                                                                            #
+# This file is part of H5Serv (HDF5 REST Server) Service, Libraries and      #
+# Utilities.  The full HDF5 REST Server copyright notice, including          #
+# terms governing use, modification, and redistribution, is contained in     #
+# the file COPYING, which can be found at the root of the source code        #
+# distribution tree.  If you do not have access to this file, you may        #
+# request a copy from help@hdfgroup.org.                                     #
+##############################################################################
+
+from __future__ import absolute_import
+import numpy
+import six
+from .base import  _decode
+from .dataset import Dataset
+from .objectid import DatasetID
+from . import selections as sel
+from .h5type import Reference
+from .h5type import check_dtype
+
+
+
+
+class Table(Dataset):
+
+    """
+        Represents an HDF5 dataset
+    """
+    def __init__(self, bind):
+        """ Create a new Table object by binding to a low-level DatasetID.
+        """
+
+        if not isinstance(bind, DatasetID):
+            raise ValueError("%s is not a DatasetID" % bind)
+        Dataset.__init__(self, bind)
+
+        if len(self._dtype) < 1:
+            raise ValueError("Table type must be compound")
+
+        if len(self._shape) > 1:
+            raise ValueError("Table must be one-dimensional")
+
+       
+    @property
+    def colnames(self):
+        """Numpy-style attribute giving the number of dimensions"""
+        names = []
+        for field in self._dtype.descr:
+            # each element should be a tuple ('fieldname', dt)
+            names.append(field[0])
+        return names
+
+    @property
+    def nrows(self):
+        return self._shape[0]
+
+    def read(self, start=None, stop=None, step=None, field=None, out=None):
+        if start is None:
+            start = 0
+        if stop is None:
+            stop = self._shape[0]
+        if step is None:
+            step = 1
+        arr = self[start:stop:step]
+        if field is not None:
+            #TBD - read just the field once the service supports it
+            tmp = arr[field]
+            arr = tmp
+        if out is not None:
+            # TBD - read direct
+            numpy.copyto(out, arr)
+        else:
+            return arr
+            
+
+
+    def read_where(self, condition, condvars=None, field=None, start=None, stop=None, step=None):
+        """Read rows from table using pytable-style condition
+        """
+        names = ()  # todo
+        def readtime_dtype(basetype, names):
+            """ Make a NumPy dtype appropriate for reading """
+
+            if len(names) == 0:  # Not compound, or we want all fields
+                return basetype
+
+            if basetype.names is None:  # Names provided, but not compound
+                raise ValueError("Field names only allowed for compound types")
+
+            for name in names:  # Check all names are legal
+                if not name in basetype.names:
+                    raise ValueError("Field %s does not appear in this type." % name)
+
+            return numpy.dtype([(name, basetype.fields[name][0]) for name in names])
+
+        new_dtype = getattr(self._local, 'astype', None)
+        if new_dtype is not None:
+            new_dtype = readtime_dtype(new_dtype, names)
+        else:
+            # This is necessary because in the case of array types, NumPy
+            # discards the array information at the top level.
+            new_dtype = readtime_dtype(self.dtype, names)
+        # todo - will need the following once we have binary transfers
+        # mtype = h5t.py_create(new_dtype)
+        mtype = new_dtype
+
+     
+
+        # Perform the dataspace selection
+        if start or stop:
+            if not start:
+                start = 0
+            if not stop:
+                stop = self._shape[0]
+        else:
+            start = 0
+            stop = self._shape[0]
+
+        selection_arg = slice(start, stop)
+        selection = sel.select(self, selection_arg)
+
+        if selection.nselect == 0:
+            return numpy.ndarray(selection.mshape, dtype=new_dtype)
+
+        # setup for pagination in case we can't read everthing in one go
+        data = []
+        cursor = start
+        page_size = stop - start
+
+        while True:
+            # Perfom the actual read
+            req = "/datasets/" + self.id.uuid + "/value"
+            params = {}
+            params["query"] = condition
+            self.log.info("req - cursor: {} page_size: {}".format(cursor, page_size))
+            end_row = cursor+page_size
+            if end_row > stop:
+                end_row = stop
+            selection_arg = slice(cursor, end_row)
+            selection = sel.select(self, selection_arg)
+
+            sel_param = selection.getQueryParam()
+            self.log.debug("query param: {}".format(sel_param))
+            if sel_param:
+                params["select"] = sel_param
+            try:
+                self.log.debug("params: {}".format(params))
+                rsp = self.GET(req, params=params)
+                count = len(rsp["value"])
+                self.log.info("got {} rows".format(count))
+                if count > 0:
+                    data.extend(rsp['value'])
+                # advance to next page
+                cursor += page_size
+            except IOError as ioe:
+                if ioe.errno == 413 and page_size > 1024:
+                    # too large a query target, try reducing the page size
+                    # if it is not already relatively small (1024)
+                    page_size //= 2
+                    page_size += 1  # bump up to avoid tiny pages in the last iteration
+                    self.log.info("Got 413, reducing page_size to: {}".format(page_size))
+                else:
+                    # otherwise, just raise the exception
+                    self.log.info("Unexpected exception: {}".format(ioe.errno))
+                    raise ioe
+            if cursor >= stop:
+                self.log.info("completed iteration, returning: {} rows".format(len(data)))
+                break
+
+        # need some special conversion for compound types --
+        # each element must be a tuple, but the JSON decoder
+        # gives us a list instead.
+
+        mshape = (len(data),)
+        if len(mtype) > 1 and type(data) in (list, tuple):
+            converted_data = []
+            for i in range(len(data)):
+                converted_data.append(self.toTuple(data[i]))
+            data = converted_data
+
+        arr = numpy.empty(mshape, dtype=mtype)
+        arr[...] = data
+
+        # Patch up the output for NumPy
+        if len(names) == 1:
+            arr = arr[names[0]]     # Single-field recarray convention
+        if arr.shape == ():
+            arr = numpy.asscalar(arr)
+
+        return arr
+    
+    def append(self, rows):
+        """ Append rows to end of table 
+        """
+        self.log.info("Table append")
+        if not self.id.uuid.startswith("d-"):
+            # Append ops only work with HSDS
+            raise ValueError("append not supported")
+
+        if self._item_size != "H5T_VARIABLE":
+            use_base64 = True   # may need to set this to false below for some types
+        else:
+            use_base64 = False  # never use for variable length types
+            self.log.debug("Using JSON since type is variable length")
+
+        val = rows  # for compatibility with dataset code...
+        # get the val dtype if we're passed a numpy array
+        val_dtype = None
+        try:
+            val_dtype = val.dtype
+        except AttributeError:
+            pass # not a numpy object, just leave dtype as None
+
+        if isinstance(val, Reference):
+            # h5pyd References are just strings
+            val = val.tolist() 
+
+        # Generally we try to avoid converting the arrays on the Python
+        # side.  However, for compound literals this is unavoidable.
+        # For h5pyd, do extra check and convert type on client side for efficiency
+        vlen = check_dtype(vlen=self.dtype)
+        if vlen is not None and vlen not in (bytes, six.text_type):
+            self.log.debug("converting ndarray for vlen data")
+            try:
+                val = numpy.asarray(val, dtype=vlen)
+            except ValueError:
+                try:
+                    val = numpy.array([numpy.array(x, dtype=vlen)
+                                       for x in val], dtype=self.dtype)
+                except ValueError:
+                    pass
+            if vlen == val_dtype:
+                if val.ndim > 1:
+                    tmp = numpy.empty(shape=val.shape[:-1], dtype=object)
+                    tmp.ravel()[:] = [i for i in val.reshape(
+                        (numpy.product(val.shape[:-1]), val.shape[-1]))]
+                else:
+                    tmp = numpy.array([None], dtype=object)
+                    tmp[0] = val
+                val = tmp
+
+        elif isinstance(val, numpy.ndarray):
+            # convert array if needed
+            # TBD - need to handle cases where the type shape is different
+            self.log.debug("got numpy array")
+            if val.dtype != self.dtype and val.dtype.shape == self.dtype.shape:
+                self.log.info("converting {} to {}".format(val.dtype, self.dtype))
+                # convert array
+                tmp = numpy.empty(val.shape, dtype=self.dtype)
+                tmp[...] = val[...]
+                val = tmp
+        else:
+            val = numpy.asarray(val, order='C', dtype=self.dtype)
+        
+        self.log.debug("rows shape: {}".format(val.shape))
+        self.log.debug("data dtype: {}".format(val.dtype))
+
+        if len(val.shape) != 1:
+            raise ValueError("rows must be one-dimensional")
+
+        numrows = val.shape[0]
+
+        req = "/datasets/" + self.id.uuid + "/value"
+
+        params = {}
+        body = {}
+
+
+        format = "json"
+
+        if use_base64:
+
+            # server is HSDS, use binary data, use param values for selection
+            format = "binary"
+            body = val.tobytes()
+            self.log.debug("writing binary data, {} bytes".format(len(body)))
+            params["append"] = numrows
+        else:
+            if type(val) is not list:
+                val = val.tolist()
+            val = _decode(val)
+            self.log.debug("writing json data, {} elements".format(len(val)))
+            self.log.debug("data: {}".format(val))
+            body['value'] = val
+            body['append'] = numrows
+
+        self.PUT(req, body=body, format=format, params=params)
+
+        # if we get here, the request was successful, adjust the shape
+        total_rows = self._shape[0] + numrows
+        self._shape = (total_rows,)
---
+++ test/test_table.py
@@ -0,0 +1,108 @@
+##############################################################################
+# Copyright by The HDF Group.                                                #
+# All rights reserved.                                                       #
+#                                                                            #
+# This file is part of H5Serv (HDF5 REST Server) Service, Libraries and      #
+# Utilities.  The full HDF5 REST Server copyright notice, including          #
+# terms governing use, modification, and redistribution, is contained in     #
+# the file COPYING, which can be found at the root of the source code        #
+# distribution tree.  If you do not have access to this file, you may        #
+# request a copy from help@hdfgroup.org.                                     #
+##############################################################################
+
+import numpy as np
+import math
+import logging
+
+import config
+
+if config.get("use_h5py"):
+    import h5py
+else:
+    import h5pyd as h5py
+
+from common import ut, TestCase
+
+
+class TestDatasetTable(TestCase):
+    def test_create_table(self):
+        filename = self.getFileName("create_table_dset")
+        print("filename:", filename)
+        if config.get("use_h5py"):
+            return # Table not supported with h5py
+        f = h5py.File(filename, "w")
+
+        count = 10
+
+        dt = np.dtype([('real', np.float), ('img', np.float)])
+        table = f.create_table('complex', numrows=10, dtype=dt)
+
+        elem = table[0]
+        for i in range(count):
+            theta = (4.0 * math.pi)*(float(i)/float(count))
+            elem['real'] = math.cos(theta)
+            elem['img'] = math.sin(theta)
+            table[i] = elem
+
+        self.assertEqual(table.colnames, ['real', 'img'])
+        self.assertEqual(table.nrows, count)
+        for row in table:
+            self.assertEqual(len(row), 2)
+        arr = table.read(start=5, stop=6)
+        self.assertEqual(arr.shape, (1,))
+
+            
+        f.close()
+
+    def test_query_table(self):
+        filename = self.getFileName("query_compound_dset")
+        print("filename:", filename)
+        if config.get("use_h5py"):
+            return # Table not supported with h5py
+        f = h5py.File(filename, "w")
+
+        #curl -v --header "Host: create_compound_dset.h5pyd_test.hdfgroup.org" http://127.0.0.1:5000
+
+        # write entire array
+        data = [
+            ("EBAY", "20170102", 3023, 3088),
+            ("AAPL", "20170102", 3054, 2933),
+            ("AMZN", "20170102", 2973, 3011),
+            ("EBAY", "20170103", 3042, 3128),
+            ("AAPL", "20170103", 3182, 3034),
+            ("AMZN", "20170103", 3021, 2788),
+            ("EBAY", "20170104", 2798, 2876),
+            ("AAPL", "20170104", 2834, 2867),
+            ("AMZN", "20170104", 2891, 2978),
+            ("EBAY", "20170105", 2973, 2962),
+            ("AAPL", "20170105", 2934, 3010),
+            ("AMZN", "20170105", 3018, 3086)
+        ] 
+         
+        dt = np.dtype([('symbol', 'S4'), ('date', 'S8'), ('open', 'i4'), ('close', 'i4')])
+        table = f.create_table('stock', dtype=dt)
+        
+        table.append(data)
+
+        self.assertEqual(table.nrows, len(data))
+
+        for indx in range(len(data)):
+            row = table[indx]
+            item = data[indx]
+            for col in range(2,3):
+                # first two columns will come back as bytes, not strs
+                self.assertEqual(row[col], item[col])
+         
+        quotes = table.read_where("symbol == b'AAPL'")
+        self.assertEqual(len(quotes), 4)
+        for i in range(4):
+            quote = quotes[i]
+            self.assertEqual(quote[0], b'AAPL')
+        f.close()
+
+
+
+if __name__ == '__main__':
+    loglevel = logging.ERROR
+    logging.basicConfig(format='%(asctime)s %(message)s', level=loglevel)
+    ut.main()
