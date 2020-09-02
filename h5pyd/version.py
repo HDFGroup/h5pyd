ddf7b863880320ca64ec4927d7a173648d822012
jreadey <jreadey@hdfgroup.org>
2020-09-02 09:31:36 -0700

    updated version

--- h5pyd/version.py
+++ h5pyd/version.py
@@ -16,7 +16,7 @@ from distutils.version import StrictVersion as _sv
 import sys
 import numpy
 
-version = "0.7.3"
+version = "0.8.0"
 
 hdf5_version = "REST"
 
@@ -30,8 +30,8 @@ version_tuple = (
     )
 )
 
-api_version_tuple = (0, 7, 3)
-api_version = "0.7.3"
+api_version_tuple = (0, 8, 0)
+api_version = "0.8.0"
 
 __doc__ = """\
 This is h5pyd **%s**
