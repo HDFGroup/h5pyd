f631f2330e9871eeabdb7fb468e6ca33fa4fadcd
jreadey <jreadey@hdfgroup.org>
2021-03-04 09:37:03 -0800

    bump version to 0.8.3

--- h5pyd/version.py
+++ h5pyd/version.py
@@ -16,7 +16,7 @@ from distutils.version import StrictVersion as _sv
 import sys
 import numpy
 
-version = "0.8.2"
+version = "0.8.3"
 
 hdf5_version = "REST"
 
@@ -30,8 +30,8 @@ version_tuple = (
     )
 )
 
-api_version_tuple = (0, 8, 2)
-api_version = "0.8.2"
+api_version_tuple = (0, 8, 3)
+api_version = "0.8.3"
 
 __doc__ = """\
 This is h5pyd **%s**
