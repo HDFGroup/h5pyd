7d104c629d9748af58196fd443b989ac6cb0f238
jreadey <jreadey@hdfgroup.org>
2022-09-25 13:44:35 -0600

    remove debug print statement

--- h5pyd/_apps/chunkiter.py
+++ h5pyd/_apps/chunkiter.py
@@ -13,12 +13,13 @@
 
 import numpy as np
 
-CHUNK_BASE = 16*1024    # Multiplier by which chunks are adjusted
-CHUNK_MIN = 8*1024      # Soft lower limit (8k)
-CHUNK_MAX = 1024*1024   # Hard upper limit (1M)
+CHUNK_BASE = 16 * 1024  # Multiplier by which chunks are adjusted
+CHUNK_MIN = 8 * 1024  # Soft lower limit (8k)
+CHUNK_MAX = 1024 * 1024  # Hard upper limit (1M)
+
 
 def guess_chunk(shape, maxshape, typesize):
-    """ Guess an appropriate chunk layout for a dataset, given its shape and
+    """Guess an appropriate chunk layout for a dataset, given its shape and
     the size of each element in bytes.  Will allocate chunks only as large
     as MAX_SIZE.  Chunks are generally close to some power-of-2 fraction of
     each axis, slightly favoring bigger values for the last index.
@@ -28,20 +29,20 @@ def guess_chunk(shape, maxshape, typesize):
     # pylint: disable=unused-argument
 
     # For unlimited dimensions we have to guess 1024
-    shape = tuple((x if x!=0 else 1024) for i, x in enumerate(shape))
+    shape = tuple((x if x != 0 else 1024) for i, x in enumerate(shape))
 
     ndims = len(shape)
     if ndims == 0:
         raise ValueError("Chunks not allowed for scalar datasets.")
 
-    chunks = np.array(shape, dtype='=f8')
+    chunks = np.array(shape, dtype="=f8")
     if not np.all(np.isfinite(chunks)):
         raise ValueError("Illegal value in chunk tuple")
 
     # Determine the optimal chunk size in bytes using a PyTables expression.
     # This is kept as a float.
-    dset_size = np.product(chunks)*typesize
-    target_size = CHUNK_BASE * (2**np.log10(dset_size/(1024.*1024)))
+    dset_size = np.product(chunks) * typesize
+    target_size = CHUNK_BASE * (2 ** np.log10(dset_size / (1024.0 * 1024)))
 
     if target_size > CHUNK_MAX:
         target_size = CHUNK_MAX
@@ -55,51 +56,57 @@ def guess_chunk(shape, maxshape, typesize):
         # 1b. We're within 50% of the target chunk size, AND
         #  2. The chunk is smaller than the maximum chunk size
 
-        chunk_bytes = np.product(chunks)*typesize
+        chunk_bytes = np.product(chunks) * typesize
 
-        if (chunk_bytes < target_size or \
-         abs(chunk_bytes-target_size)/target_size < 0.5) and \
-         chunk_bytes < CHUNK_MAX:
+        if (
+            chunk_bytes < target_size
+            or abs(chunk_bytes - target_size) / target_size < 0.5
+        ) and chunk_bytes < CHUNK_MAX:
             break
 
         if np.product(chunks) == 1:
             break  # Element size larger than CHUNK_MAX
 
-        chunks[idx%ndims] = np.ceil(chunks[idx%ndims] / 2.0)
+        chunks[idx % ndims] = np.ceil(chunks[idx % ndims] / 2.0)
         idx += 1
 
     return tuple(int(x) for x in chunks)
-    
+
+
 class ChunkIterator:
     """
     Class to iterate through list of chunks given h5py dset
     """
+
     def __init__(self, dset):
         self._shape = dset.shape
         if dset.chunks is None:
-             # guess a chunk shape
-            self._layout =  guess_chunk(self._shape, None, dset.dtype.itemsize)
-            print(f"guess chunk: {self._layout}")
+            # guess a chunk shape
+            self._layout = guess_chunk(self._shape, None, dset.dtype.itemsize)
         elif isinstance(dset.chunks, dict):
             self._layout = dset.chunks["dims"]
         else:
             self._layout = dset.chunks
         self._rank = len(dset.shape)
         if self._rank == 0:
-            self._chunk_index = [0,]
+            self._chunk_index = [
+                0,
+            ]
         else:
-            self._chunk_index = [0,] * self._rank
+            self._chunk_index = [
+                0,
+            ] * self._rank
 
     def __iter__(self):
         return self
 
     def __next__(self):
-
         def get_ret(item):
             if len(item) == 1:
                 return item[0]
             else:
                 return tuple(item)
+
         if self._layout == ():
             # special case for scalar datasets
             if self._chunk_index[0] > 0:
