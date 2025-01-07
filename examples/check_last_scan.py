d70f6344c107150f76e096b861f7718554a19c41
John Readey <jreadey@hdfgroup.org>
2025-01-07 15:46:44 +0800

    fix flake8 error

--- examples/check_last_scan.py
+++ examples/check_last_scan.py
@@ -10,7 +10,7 @@ f.create_group("g1")  # create a new group
 ts = time.time()
 print("waiting for scan update")
 while f.last_scan == time_stamp:
-   time.sleep(0.1)  # wait for summary data to be updated
+    time.sleep(0.1)  # wait for summary data to be updated
 wait_time = time.time() - ts
 print(f"last_scan updated after: {wait_time:6.2f} seconds")
 # print affected summary properties
