b92c091cb6d208f7016238695c421cbac76c9c5a
woutdenolf <woutdenolf@users.sf.net>
2026-06-20 08:34:44 +0200

    remove deprecated Version._version call and support everything except for epoch

--- h5pyd/version.py
+++ h5pyd/version.py
@@ -12,7 +12,7 @@
 
 from __future__ import absolute_import
 
-from packaging.version import Version, parse
+from packaging.version import parse
 import sys
 import numpy
 
@@ -20,13 +20,25 @@ version = "0.24.0"
 
 hdf5_version = "REST"
 
-_exp = parse(version)
+_pversion = parse(version)
 
-version_tuple = _exp._version + (
-    ("".join(str(x) for x in _exp.pre),)
-    if _exp.is_prerelease
-    else ("",)
-)
+_suffix = ""
+if _pversion.pre is not None:
+    _suffix += "".join(str(x) for x in _pversion.pre)
+if _pversion.post is not None:
+    if _suffix:
+        _suffix += "."
+    _suffix += f"post{_pversion.post}"
+if _pversion.dev is not None:
+    if _suffix:
+        _suffix += "."
+    _suffix += f"dev{_pversion.dev}"
+if _pversion.local is not None:
+    if _suffix:
+        _suffix += "+"
+    _suffix += str(_pversion.local)
+
+version_tuple = (_pversion.major, _pversion.minor, _pversion.micro, _suffix)
 
 api_version_tuple = (0, 24, 0)
 api_version = "0.24.0"
